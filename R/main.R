0de63bcc6081478a1c694b0c47249f8fcf7d5112
michi <michi@babymac.basilisk-buri.ts.net>
2025-08-03 06:56:23 +0200

    main script nicer outlay

--- R/main.R
+++ R/main.R
@@ -1,6 +1,4 @@
-################################################################################
 # main.R                                              (c) J.M.B. Koch 2022
-################################################################################
 # This is the main script running the simulation 
 # Dependencies: functions.R; parameters.R; 
 
@@ -13,7 +11,9 @@ if (!as.package_version(cmdstanr::cmdstan_version()) <= as.package_version("2.34
   cli::cli_abort("cmdstanr version must be 2.34.0 (or lower) due to this bug: {.url https://github.com/stan-dev/rstan/issues/1133?utm_source=chatgpt.com}")
 }
 
-# Prepare data SVNP ------------------------------------------------------------
+# SVNP  ------------------------------------------------------------
+
+## Prepare data SVNP ------------------------------------------------------------
 # simulate data
 if (!file.exists('data/datasets.RDS')){
   datasets <- simDatasets(condPop = condPop, modelPars = modelPars, nIter = nIter)
@@ -72,7 +72,9 @@ endTimeSVNP <- Sys.time()
 elapsedTimesSVNP <- endTimeSVNP-startTimeSVNP
 elapsedTimesSVNP
 
-# Prepare data for SVNP wishart ------------------------------------------------------------
+
+# SVNP wishart ------------------------------------------------------------
+## Prepare data for SVNP wishart ------------------------------------------------------------
 # We can simply transform all simulated datasets to the empirical covariance matrix
 dataStanSVNP_wishart <- purrr::imap(dataStanSVNP, 
                                    ~ { .x$S <- cov(.x$Y) 
@@ -80,16 +82,9 @@ dataStanSVNP_wishart <- purrr::imap(dataStanSVNP,
                                        return(.x)
                                       } )
 
-## Execute simulation for SVNP wishart ---------------------------------------------
-# breaks
-if (file.exists("output/resultsSVNP_wishart.RDS")){
-  stop("output already exists. Please remove or backup before proceeding.")
-} else if (length(dataStanSVNP_wishart) != (nIter*nrow(condSVNP) * nrow(condPop))){
-  stop("something went wrong with simulating the data!")
-}
 
-# do the sampling where every available core (nWorkers in condtions.R) does
-#    one unique combination of conditions
+## Execute simulation for SVNP wishart ---------------------------------------------
+
 # measure start time
 startTimeSVNP_wishart <- Sys.time()
 # create clusters
@@ -102,12 +97,13 @@ clusterCall(clusters,
 # Load packages per cluster
 clusterCall(clusters,
             function() lapply(packages, library, character.only = TRUE))
-# read in stan-ready data within clusters
-clusterCall(clusters,
-            function() dataStanSVNP_wishart <- readr::read_rds("data/dataStanSVNP_wishart.RDS"))
+
+# export stan-ready data to cluster scope
+clusterExport(clusters,
+             varlist = c("dataStanSVNP_wishart"))
 # run function clustered over individual combo's of
 #  iteration, condPop and condPrior
-outputFinalSVNP_hyper  <- clusterApplyLB(clusters,
+outputFinalSVNP_wishart <- clusterApplyLB(clusters,
                                          1:length(dataStanSVNP_wishart),
                                          sampling,
                                          dataStan = dataStanSVNP_wishart,
@@ -120,19 +116,11 @@ stopCluster(clusters)
 # measure end time
 endTimeSVNP_wishart  <- Sys.time()
 # measure elapsed time
-elapsedTimesSVNP_wishart<- endTimeSVNP_wishart-startTimeSVNP_wishart
+elapsedTimesSVNP_wishart <- endTimeSVNP_wishart-startTimeSVNP_wishart
 elapsedTimesSVNP_wishart
 
-
-# # Prepare data SVNP hyper ------------------------------------------------------------
-# simulate data
-if (!file.exists('data/datasets.RDS')){
-  datasets <- simDatasets(condPop = condPop, modelPars = modelPars, nIter = nIter)
-  # save raw data
-  readr::write_rds(datasets, file = 'data/datasets.RDS ' )
-}else {
-  datasets <- readr::read_rds('data/datasets.RDS')
-}
+# SVNP hyper ------------------------------------------------------------
+## Prepare data SVNP hyper ------------------------------------------------------------
 # prepare data for stan
 if (!file.exists('data/dataStanSVNP_hyper.RDS')){
   dataStanSVNP_hyper <- prepareDat(datasets, condSVNP_hyper, nIter)
@@ -186,7 +174,56 @@ endTimeSVNP_hyper  <- Sys.time()
 elapsedTimesSVNP_hyper <- endTimeSVNP_hyper-startTimeSVNP_hyper
 elapsedTimesSVNP_hyper
 
-# Prepare data voor RHSP ---------------------------------------------
+
+# SVNP hyper wishart ------------------------------------------------------
+## prepare data voor SVNP hyper wishart ------------------------------------
+dataStanSVNP_hyper_wishart <- purrr::imap(dataStan_hyper, 
+                                      ~ { .x$S <- cov(.x$Y) 
+                                      .x$Y <- NULL
+                                      return(.x)
+                                      } )
+
+
+## Execute simulation for SVNP hyper wishart ---------------------------------------------
+
+# measure start time
+startTimeSVNP_hyper_wishart <- Sys.time()
+
+# create clusters
+clusters <- makePSOCKcluster(nClusters)
+# source functions & parameters within clusters
+clusterCall(clusters,
+            function() source('R/functions.R'))
+clusterCall(clusters,
+            function() source('R/parameters.R'))
+# Load packages per cluster
+clusterCall(clusters,
+            function() lapply(packages, library, character.only = TRUE))
+# export stan-ready data to cluster scope
+clusterExport(clusters,
+              varlist = c("dataStanSVNP_hyper_wishart"))
+# run function clustered over individual combo's of
+#  iteration, condPop and condPrior
+outputFinalSVNP_hyper_wishart  <- clusterApplyLB(clusters,
+                                         1:length(dataStanSVNP_hyper_wishart),
+                                         sampling,
+                                         dataStan = dataStanSVNP_hyper_wishart,
+                                         prior = "SVNP_hyper",
+                                         modelPars = modelPars,
+                                         samplePars = samplePars,
+                                         wishart = TRUE)
+# close clusters
+stopCluster(clusters)
+# measure end time
+endTimeSVNP_hyper_wishart  <- Sys.time()
+# measure elapsed time
+elapsedTimesSVNP_hyper_wishart <- endTimeSVNP_hyper_wishart-startTimeSVNP_hyper_wishart
+elapsedTimesSVNP_hyper_wishart
+
+
+# RHSP --------------------------------------------------------------------
+
+## Prepare data voor RHSP ---------------------------------------------
 
 # load data if it exists, else make it
 # if (file.exists("data/dataStanRHSP.RDS")) {
@@ -230,7 +267,8 @@ elapsedTimesSVNP_hyper
 #                                  dataStan = dataStanRHSP,
 #                                  prior = "RHSP",
 #                                  modelPars = modelPars,
-#                                  samplePars = samplePars)
+#                                  samplePars = samplePars,
+#                                  wishart = FALSE)
 # # close clusters
 # stopCluster(clusters)
 # # measure end time
@@ -238,7 +276,11 @@ elapsedTimesSVNP_hyper
 # #measure elapsed time
 # elapsedTimesRHSP <- endTimeRHSP-startTimeRHPS
 
-# Prepare data voor RHSP wishart ---------------------------------------------
+
+# RHSP wishart ------------------------------------------------------------
+
+
+## Prepare data voor RHSP wishart ---------------------------------------------
 # dataStanSVNP_wishart <- purrr::imap(dataStanRHSP, 
 #                                     ~ { .x$S <- cov(.x$Y) 
 #                                     .x$Y <- NULL
@@ -259,9 +301,9 @@ elapsedTimesSVNP_hyper
 # # Load packages per cluster
 # clusterCall(clusters,
 #             function() lapply(packages, library, character.only = TRUE))
-# # read in stan-ready data within clusters
-# clusterCall(clusters,
-#             function() load("~/1vs2StepBayesianRegSEM/data/dataStanRHSP_wishart.RDS"))
+# export stan-ready data to cluster scope
+# clusterExport(clusters,
+#               varlist = c("dataStanRHSP_wishart"))
 # 
 # # run functon in clustered way where it's clustered over individual combo's of
 # #  iteration, condPop and condPrior
