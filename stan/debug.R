fbff222fc47e884da51cfc1c561d843c3b1c9dae
michi <michi@babymac.basilisk-buri.ts.net>
2025-08-03 12:31:51 +0200

    RHSP can be run in clusters with wishart'

--- stan/debug.R
+++ stan/debug.R
@@ -4,8 +4,21 @@ source('R/parameters.R')
 
 model <- cmdstan_model("stan/RHSP_wishart.stan")
 
-model_normal <- cmdstan_model("stan/RHSP.stan")
+datCurrent <- dataStanRHSP_wishart[[23]]
 samples <- model$sample(data = datCurrent,
+                        chains = samplePars$nChain, 
+                        iter_warmup = samplePars$nWarmup,
+                        iter_sampling = samplePars$nSampling)
+
+read_stan_csv(samples$output_files())
+
+
+model_normal <- cmdstan_model("stan/RHSP.stan")
+
+datCurrent_normal <- dataStanRHSP[[23]]
+
+
+samples_normal <- model_normal$sample(data = datCurrent_normal,
                         chains = samplePars$nChain, 
                         iter_warmup = samplePars$nWarmup,
                         iter_sampling = samplePars$nSampling)
