23cd0f1da0533626fff21fd97427b9d1dcc5db35
michi <michi@babymac.basilisk-buri.ts.net>
2025-09-21 18:51:31 +0200

    parameters recovered

--- R/parameters.R
+++ R/parameters.R
@@ -5,14 +5,11 @@
 # setting seed for reproducibility ----------------------------------------
 set.seed(0704)
 
-# Parallelization Parameter -----------------------------------------------
-# Hoeft voor testrun niet aangepast te worden
-nClusters <- 6 # depending on machine, original study run with 12 for SVNP and 46 for RHSP
-
 # Model--------------------------------------------------------------------
 # Lambda
 main <- c(.75, .75, .75, .75, .75, .75)
-cross2 <- c(.2, 0, 0, 0, 0, .2)
+cross2 <- c(.2, 0, 0, 0, 0, .2) # TBA: make this more generalizable?
+cross5 <- c(.5, 0, 0, 0, 0, .5)
 # Psi
 Psi <- matrix(rep(NA, 4), ncol = 2)
 diag(Psi) <- 1
@@ -23,25 +20,28 @@ Theta <- diag(rep(0.3, 6))
 modelPars <- list(
                 main = main,
                 cross2 = cross2,
+                cross5 = cross5,
                 Psi = Psi,
                 Theta = Theta
                   )
 
 # Hyper-Parameters: -------------------------------------------------------
 # Small Variance Normal Prior fixed ---------------------------------------------
-sigma <- c(sqrt(0.1))
+sigma <- c(sqrt(0.1), # specified such that sigma^2 > sigma
+           sqrt(0.01), 
+           sqrt(0.001))
 
 # Regularized Horseshoe Prior ---------------------------------------------
-scaleGlobal <- c(1) # scale for half-t prior omega
-scaleLocal <- c(1) # scale for half-t prior tau_j
-dfGlobal <- c(1) # df for half-t prior omega
-dfLocal <- c(1) # df for half-t prior tau_j
-nu <- c(1) # df IG for c^2 (slab)
-scaleSlab <- c(1) # scale of slab
+scaleGlobal <- c(0.1, 1) # scale for half-t prior omega
+scaleLocal <- c(0.1, 1) # scale for half-t prior tau_j
+dfGlobal <- c(1, 3) # df for half-t prior omega
+dfLocal <- c(1, 3) # df for half-t prior tau_j
+nu <- c(1, 3) # df IG for c^2 (slab)
+scaleSlab <- c(0.1, 1, 5) # scale of slab
 
 # Population conditions ----------------------------------------------------
-N <- c(100)
-cross <- c(0.2)
+N <- c(100, 200, 500)
+cross <- c(0.01, 0.1, 0.2, 0.5)
 
 # Making condition objects ------------------------------------------------
 condPop   <- 
@@ -78,7 +78,19 @@ samplePars <- list(
                 nSampling = 4000
                 )
 
+# Parallelization Parameter -----------------------------------------------
+nClusters <- 6 # depending on machine, original study run with 12 for SVNP and 46 for RHSP
+
 # other study parameters --------------------------------------------------
-nIter <- 2 # in paper(s) Iterations are referred to as "Replications"
+nIter <- 200 # in paper(s) Iterations are referred to as "Replications"
+
+# Convergence Criteria ----------------------------------------------------
+convCriteria <- list(
+  strict = list(
+    minPropNEFF = .10,
+    maxRhat = 1.05,
+    maxPropDIV = .05
+  )
+)
 
 
