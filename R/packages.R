2fcca9126be72a9486e9cf059bf425a5b1865d41
michi <michi@babymac.basilisk-buri.ts.net>
2025-10-05 16:31:30 +0200

    packages.R only handles installatino of CMDstan backend and loading of pacakges

--- R/packages.R
+++ R/packages.R
@@ -3,28 +3,6 @@ pkgRequ <- c(
   "rstan", "tidyverse", "mvtnorm", "parallel", "bayesplot", "here", "cmdstanr", "furrr"
 )
 
-# missing packages
-
-pkgGithubLookup <- list('cmdstanr' = "stan-dev/cmdstanr")
-
-# Only replace if the package is actually missing
-for (pkg in names(pkgGithubLookup)) {
-  if (pkg %in% missing) {
-    idx <- which(missing == pkg)
-    missing[idx] <- pkgGithubLookup[[pkg]]
-  }
-}
-
-# github packages lookup
-pkgGithubLookup <- list('cmdstanr' =  "stan-dev/cmdstanr")
-# overwrite missing github names with their repo
-missing[which(missing == names(pkgGithubLookup))] <- pkgGithubLookup[which(missing == names(pkgGithubLookup))]
-
-if (length(missing) >= 1) {
-  message("Installing missing packages ...")
-  renv::install(missing, prompt = FALSE)
-}
-
 # ---- CmdStan installation if needed ----
 cmdstanVersionReq <- "2.34.0"
 cmdstanVersionInstalled <- tryCatch(
